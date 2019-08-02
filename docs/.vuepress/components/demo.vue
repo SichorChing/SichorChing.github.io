b5d78df8d78bb997b78cee91f8dec2be3934e40a
chengshichu <chengshichu@aikuicun.com>
2019-08-02 17:17:55 +0800

    change file

---
+++ docs/.vuepress/components/demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    demo
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    };
+  },
+
+  components:{},
+
+  computed(){},
+
+  watch: {},
+
+  mounted(){},
+
+  methods:{}
+}
+
+</script>
+<style scoped>
+</style>
